ac5ffb44236eac3aa440a131735c7d5f821f494e
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-03-29 18:10:25 -0300

    Criando entidade Pet.

---
+++ src/main/java/br/com/zup/edu/pets/api/model/Pet.java
@@ -0,0 +1,44 @@
+package br.com.zup.edu.pets.api.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoPet tipo;
+
+    @Column(nullable = false)
+    private String raca;
+
+    public Pet(String nome, LocalDate dataNascimento, TipoPet tipo, String raca) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.tipo = tipo;
+        this.raca = raca;
+    }
+
+    /**
+     * @deprecated Construtor para uso exclusivo do Hibernate.
+     */
+    @Deprecated
+    public Pet() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+}
